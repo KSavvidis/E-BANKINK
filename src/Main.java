f3e678b811d14a7418bbf40585bc4df863483117
KSavvidis <savas.karakalos@gmail.com>
2025-05-06 01:17:31 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String nigga;
+        try(Scanner sc = new Scanner(System.in)) {
+            boolean exit = false;
+            System.out.println("Insert a string: ");
+            while(!exit){
+                if(sc.hasNext()){
+                    nigga = sc.next();
+                    if(nigga.equals("exit")){
+                        exit = true;
+                    }
+                    System.out.println(nigga);
+                }
+                else {
+                    System.out.println("Invalid input");
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error:" + e.getMessage());
+        }
+
+
+
+
 
     }
 }
