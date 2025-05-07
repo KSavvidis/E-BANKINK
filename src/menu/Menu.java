b057903348441e0136352da2e6bf806d3aad7b41
KSavvidis <savas.karakalos@gmail.com>
2025-05-07 21:07:50 +0300

    main

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,21 +1,35 @@
 package menu;
 
+import manager.UserManager;
 import java.util.Scanner;
 
 public class Menu {
     private void start(){
+        System.out.println("Welcome to the TUC Bank Menu");
+        System.out.println("===================================");
+        System.out.println("1. Login");
+        System.out.println("2. Exit");
+        System.out.println("===================================");
+        try(Scanner sc = new Scanner(System.in)){
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1:
 
+            }
+        }
     }
 
     private void showIndividualMenu(){
-        System.out.println("1.Overview");
-        System.out.println("2.Withdraw");
-        System.out.println("3.Deposit");
-        System.out.println("4.Transfer");
-        System.out.println("5.Create Standing Order");
-        System.out.println("6.List Standing Orders");
-        System.out.println("7.Back to main menu");
-        System.out.println("8.Exit");
+        System.out.println("Individual Customer Menu");
+        System.out.println("======================");
+        System.out.println("1. Overview");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Deposit");
+        System.out.println("4. Transfer");
+        System.out.println("5. Create Standing Order");
+        System.out.println("6. List Standing Orders");
+        System.out.println("7. Back to main menu");
+        System.out.println("8. Exit");
         System.out.print("Enter your choice:");
         try(Scanner sc = new Scanner(System.in)){
             if(sc.hasNextInt()){
@@ -24,6 +38,9 @@ public class Menu {
                     case 1:
 
                 }
+            }
+            else{
+                System.out.println("Please enter a valid choice. Try again.");
             }
         }
         catch(Exception e){
